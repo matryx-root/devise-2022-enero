123d74172f4f12a6b088d9445471002d50cf129e
Simon Velasquez <root.matryx@gmail.com>
2022-03-12 17:20:56 -0300

    ultimo cambio

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
+#require 'faker'
 
 puts "Cleaning database..."
 Terrain.destroy_all
 
-puts 'Creating 1000 fake Terrain...'
-1000.times do
-  terrains = Terrain.new(
-  title:    Faker::Movie.title,
-  description: Faker::Food.description,
-  value:  rand(1..10)
+#puts 'Creating 1000 fake Terrain...'
+#1000.times do
+  #terrains = Terrain.new(
+  #title:    Faker::Movie.title,
+  #description: Faker::Food.description,
+  #value:  rand(1..10)
 
-  )
-  terrains.save!
-end
+  #)
+  #terrains.save!
+#end
 puts 'Finished!'
